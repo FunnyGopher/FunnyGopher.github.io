56d960236be735854071b8e8e2685b7cd5037855
Kyle Huntsman <kyle.huntsman@meltmedia.com>
2016-02-16 10:38:31 -0700

    Now using bower dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON('package.json'),
 		connect: {
 		    server: {
 				options: {
